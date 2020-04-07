d14a8f287179c7e1fd67cc6d7f34fa3a745ef401
nursen <nursenokenov57@gmail.com>
2020-04-08 02:32:00 +0600

    New components Layout, Drawer , Toolbar

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Toolbar from "./Toolbar/Toolbar"
+import Drawer from "./Drawer/Drawer"
+
+export default ({ children }) => (
+    <div className="Layout">
+        <Toolbar />
+        <Drawer />
+        {children}
+    </div>
+);
