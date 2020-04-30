5d6d943a5d815d9cf33f63722ba154cdae8af0ea
nursen <nursenokenov57@gmail.com>
2020-05-01 00:45:55 +0600

    Add DrawerToggle component to open Drawer

---
+++ src/components/Layout/Drawer/DrawerToggle/DrawerToggle.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import classes from './DrawerToggle.module.css';
+
+export default ({ toggleDrawer }) => (
+  <div className={classes.DrawerToggle} onClick={toggleDrawer}>
+    Menu
+  </div>
+); 
--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -5,11 +5,15 @@ import classes from "./Layout.module.css"
 
 export default ({ children }) => {
     const [drawerOpen, setDrawerOpen] = useState(false);
+
+    function toggleDrawer() {
+      setDrawerOpen(drawerOpen);
+    }
   
     return (
       <div className={classes.Layout}>
-        <Toolbar />
-        <Drawer open={drawerOpen} toggleDrawer={() => {}} />
+        <Toolbar toggleDrawer={toggleDrawer}  />
+        <Drawer open={drawerOpen} toggleDrawer={toggleDrawer} />
         <main>{children}</main>
       </div>
     );
