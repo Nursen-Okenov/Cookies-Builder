e1d5d6a40b15786a2355f851e367bfb54565dae2
nursen <nursenokenov57@gmail.com>
2020-05-01 00:49:04 +0600

    Stylize DrawerToggle

--- src/components/Layout/Drawer/DrawerToggle/DrawerToggle.js
+++ src/components/Layout/Drawer/DrawerToggle/DrawerToggle.js
@@ -3,6 +3,8 @@ import classes from './DrawerToggle.module.css';
 
 export default ({ toggleDrawer }) => (
   <div className={classes.DrawerToggle} onClick={toggleDrawer}>
-    Menu
+    <div></div>
+    <div></div>
+    <div></div>
   </div>
 ); 
