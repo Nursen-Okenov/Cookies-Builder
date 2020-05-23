87a9f1af86ebed3511f831b53fa9d4254f4c83e2
nursen <nursenokenov57@gmail.com>
2020-05-24 04:51:03 +0600

    Add routes

--- src/App.js
+++ src/App.js
@@ -3,13 +3,23 @@ import Layout from "./containers/Layout/Layout";
 import CookiesBuilder from "./containers/CookiesBuilder/CookiesBuilder";
 import "./App.css";
 import Checkout from "./containers/Checkout/Checkout";
+import { Route, Switch, Redirect } from "react-router-dom";
 
 export default () => {
   return (
     <div className="App">
       <Layout>
-        <CookiesBuilder />
-        <Checkout />
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/builder" />
+          </Route>
+          <Route path="/builder">
+            <CookiesBuilder />
+          </Route>
+          <Route path="/checkout">
+            <Checkout />
+          </Route>
+        </Switch>
       </Layout>
     </div>
   );
