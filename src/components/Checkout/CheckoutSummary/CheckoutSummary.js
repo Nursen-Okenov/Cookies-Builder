b45c2524f9ef025507f4059919da07eec8399f0a
nursen <nursenokenov57@gmail.com>
2020-05-24 04:26:45 +0600

    Add buttons in the CheckoutSummary component

--- src/components/Checkout/CheckoutSummary/CheckoutSummary.js
+++ src/components/Checkout/CheckoutSummary/CheckoutSummary.js
@@ -1,11 +1,14 @@
 import React from "react";
 import classes from "./CheckoutSummary.module.css";
 import CookiesKit from "../../CookiesBuilder/CookiesKit/CookiesKit";
+import Button from "../../UI/Button/Button";
 
 export default ({ ingredients, price }) => {
   return (
     <div className={classes.CheckoutSummary}>
       <CookiesKit ingredients={ingredients} price={price} />
+      <Button red>Cancel</Button>
+      <Button green> Continue</Button>
     </div>
   );
 };
