fb21643f7aaf3bde06d8f271b43b6119b22b9eda
nursen <nursenokenov57@gmail.com>
2020-05-25 05:21:48 +0600

    Hide buttons based on route

--- src/components/Checkout/CheckoutSummary/CheckoutSummary.js
+++ src/components/Checkout/CheckoutSummary/CheckoutSummary.js
@@ -2,13 +2,22 @@ import React from "react";
 import classes from "./CheckoutSummary.module.css";
 import CookiesKit from "../../CookiesBuilder/CookiesKit/CookiesKit";
 import Button from "../../UI/Button/Button";
+import { Route } from "react-router-dom";
 
 export default ({ ingredients, price, checkoutCancel, checkoutContinue }) => {
   return (
     <div className={classes.CheckoutSummary}>
       <CookiesKit ingredients={ingredients} price={price} />
-      <Button click={checkoutCancel} red>Cancel</Button>
-      <Button click={checkoutContinue} green> Continue</Button>
+
+      <Route path="/checkout" exact>
+        <Button click={checkoutCancel} red>
+          Cancel
+        </Button>
+        <Button click={checkoutContinue} green>
+          {" "}
+          Continue
+        </Button>
+      </Route>
     </div>
   );
 };
