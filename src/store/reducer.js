e6ccfa909f0402b04700957a022a8e5ebc8f0c13
nursen <nursenokenov57@gmail.com>
2020-06-06 00:39:08 +0600

    Add actions types

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,8 +1,19 @@
+import * as actions from "./actions";
+
 const initailState = {
   ingredients: null,
   price: 50,
 };
 
 export default (state = initailState, action) => {
-  return state;
+  switch (action.type) {
+    case actions.ADD_INGREDIENTS:
+      return state;
+
+    case actions.REMOVE_INGREDIENTS:
+      return state;
+
+    default:
+      return state;
+  }
 };
