74eacccf001363647678a1e5903045f663b914d9
nursen <nursenokenov57@gmail.com>
2020-06-08 04:25:10 +0600

    Store token in a separate key

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,18 +1,29 @@
 import { AUTH_SUCCESS, AUTH_FAIL, AUTH_START } from "./types";
 import axios from "axios";
 
-export const start = (dispatch) => dispatch({
-  type: AUTH_START
-});
+export const start = (dispatch) =>
+  dispatch({
+    type: AUTH_START,
+  });
 
-export const success = (dispatch, data) => dispatch({
-  type: AUTH_SUCCESS, data
-});
+export const success = (dispatch, { idToken, localId }) =>
+  dispatch({
+    type: AUTH_SUCCESS,
+    id: idToken,
+    token: localId,
+  });
 
-export const fail = (dispatch, errors) => dispatch({
-  type: AUTH_FAIL, errors
-});
+export const fail = (dispatch, errors) =>
+  dispatch({
+    type: AUTH_FAIL,
+    errors,
+  });
 
-export const auth = (dispatch, email, password) => axios.post("https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAo_6OwVQr5zI47ZHPiI21Y3RwQGpRwl5w" , { email, password })
-  .then(({ data }) => success(dispatch, data))
-  .catch(error => fail(dispatch, error)); 
+export const auth = (dispatch, email, password) =>
+  axios
+    .post(
+      "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAo_6OwVQr5zI47ZHPiI21Y3RwQGpRwl5w",
+      { email, password }
+    )
+    .then(({ data }) => success(dispatch, data))
+    .catch((error) => fail(dispatch, error));
